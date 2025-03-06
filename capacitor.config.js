7b4a6896a616848789fe92ddabe2423dc2cea94c
ianmugo1 <b00143913@mytudublin.ie>
2025-03-06 12:48:37 +0000

    Add Capacitor config and iOS platform files; remove obsolete API route

---
+++ capacitor.config.js
@@ -0,0 +1,8 @@
+const config = {
+  appId: 'com.example.courseconnect',
+  appName: 'Course Connect',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+module.exports = config;
--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 // next.config.js
 const withPWA = require("next-pwa")({
   dest: "public",
-  // Disable PWA support in development for easier debugging
   disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
-  // Other Next.js config options can go here.
+  // No static export:  app will be deployed as a full-stack app.
 });
